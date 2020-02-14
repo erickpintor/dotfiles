9ac0ecd02059b03fd71aaf79d3c87a18267cbf02
Erick Pintor <erickpintor@gmail.com>
2020-02-14 15:11:20 -0300

    include tigrc config

--- install.sh
+++ install.sh
@@ -5,25 +5,26 @@ set -e
 ROOT="$HOME/.dotfiles"
 
 function main {
-    git pull --rebase
+    echo "Linking config files..."
 
     folder "$HOME/.config/alacritty"
     folder "$HOME/.config/fish/functions"
     folder "$HOME/.ssh"
 
-    link "$ROOT/vim"                   "$HOME/.vim"
-    link "$ROOT/config/ctags"          "$HOME/.ctags"
-    link "$ROOT/config/gitconfig"      "$HOME/.gitconfig"
-    link "$ROOT/config/gitignore"      "$HOME/.gitignore"
-    link "$ROOT/config/inputrc"        "$HOME/.inputrc"
-    link "$ROOT/config/sbt_config"     "$HOME/.sbt_config"
-    link "$ROOT/config/screenrc"       "$HOME/.screenrc"
-    link "$ROOT/config/ssh_config"     "$HOME/.ssh/config"
-    link "$ROOT/config/tmux.conf"      "$HOME/.tmux.conf"
-    link "$ROOT/config/alacritty.yml"  "$HOME/.config/alacritty/alacritty.yml"
-    link "$ROOT/fish/functions/g.fish" "$HOME/.config/fish/functions/g.fish"
+    link "$ROOT/vim"                    "$HOME/.vim"
+    link "$ROOT/config/ctags"           "$HOME/.ctags"
+    link "$ROOT/config/gitconfig"       "$HOME/.gitconfig"
+    link "$ROOT/config/gitignore"       "$HOME/.gitignore"
+    link "$ROOT/config/inputrc"         "$HOME/.inputrc"
+    link "$ROOT/config/sbt_config"      "$HOME/.sbt_config"
+    link "$ROOT/config/screenrc"        "$HOME/.screenrc"
+    link "$ROOT/config/ssh_config"      "$HOME/.ssh/config"
+    link "$ROOT/config/tmux.conf"       "$HOME/.tmux.conf"
+    link "$ROOT/config/tigrc"           "$HOME/.tigrc"
+    link "$ROOT/config/alacritty.yml"   "$HOME/.config/alacritty/alacritty.yml"
+    link "$ROOT/fish/functions/g.fish"  "$HOME/.config/fish/functions/g.fish"
 
-    echo "Everything installed!"
+    echo "Done!"
 }
 
 function folder {
